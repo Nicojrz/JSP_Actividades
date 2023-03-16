80efd2f1dd1f1d9fdb5ae49d302ce753092078d4
Nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2023-03-15 21:56:05 -0600

    Public-1.0

---
+++ src/java/source/Datos.java
@@ -0,0 +1,31 @@
+package source;
+
+/**
+ * By Nicojrz
+ */
+
+import java.io.Serializable;
+
+public class Datos implements Serializable {
+    
+    private String name;
+    private float grade;
+
+    public Datos() {}
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+}
---
+++ src/java/source/Negocio.java
@@ -0,0 +1,42 @@
+package source;
+
+/**
+ * By Nicojrz
+ */
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Negocio implements Serializable {
+    
+    private List<Datos> Lista;
+
+    public Negocio() {}
+    
+    public boolean loadLista()
+    {
+        Datos datos = null;
+        Lista = new ArrayList<>();
+        for(int i = 1; i <= 5; i++) {
+            datos = new Datos();
+            datos.setGrade((float)(Math.random()*10));
+            datos.setName(String.format("Alumno %d", i));
+            Lista.add(datos);
+        }
+        return Lista != null && !Lista.isEmpty();
+    }
+    
+    public List<Datos> getLista() {
+        if(Lista == null || Lista.isEmpty()) {
+            if(!loadLista()) {
+                return null;
+            }
+        }
+        return Lista;
+    }
+
+    public void setLista(List<Datos> Lista) {
+        this.Lista = Lista;
+    }
+}
